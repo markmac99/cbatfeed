759919d51dea196a21b01a423db56b05b97a07f7
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-12-04 15:39:14 +0000

    source the config file, doh

--- cbatFeed.sh
+++ cbatFeed.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
 source ~/venvs/CBATfeed/bin/activate
+source $here/config.ini
 
 cd $here
 # curl http://www.cbat.eps.harvard.edu/unconf/tocp.xml > newtocp.xml
+python $here/cbatFeed.py
 
-python cbatFeed.py
-
-#cp cbattocp.html ~/data
 scp -i $SSHKEY ./cbattocp.html $USERNAME@$HOSTNAME:$DESTDIR
