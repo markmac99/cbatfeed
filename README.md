96c4f740b23a7b72f1f65066f4c0752f51fb3516
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-12-04 14:23:38 +0000

    migrate off wordpress server

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 README
+======
+To install: 
+clone the git repository
+create a new python virtualenv with python 3.7 or later
+activate the virtualenv
+run pip install -r requirements.txt
--- cbatFeed.sh
+++ cbatFeed.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
- 
+
+source ~/venvs/CBATfeed/bin/activate
+
 cd $here
 # curl http://www.cbat.eps.harvard.edu/unconf/tocp.xml > newtocp.xml
 
 python cbatFeed.py
 
-cp cbattocp.html ~/data
+#cp cbattocp.html ~/data
+scp -i $SSHKEY ./cbattocp.html $USERNAME@$HOSTNAME:$DESTDIR
